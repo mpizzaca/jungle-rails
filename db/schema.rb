5b9a5e59baeb62b382ac15a7e8ba5bbb5ff4a170
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-03 16:54:23 -0400

    add user tests, additional user validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210429165224) do
+ActiveRecord::Schema.define(version: 20210503200944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,11 @@ ActiveRecord::Schema.define(version: 20210429165224) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
