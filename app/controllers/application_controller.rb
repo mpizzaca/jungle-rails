ff51606b722d483f1e763c95f1635796fb14eb04
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-05 14:48:01 -0400

    show discounted price in cart, total

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SalesHelper
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -21,12 +22,14 @@ class ApplicationController < ActionController::Base
   helper_method :cart
 
   def enhanced_cart
-    @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+    products = Product.where(id: cart.keys)
+    products_with_discounts = apply_discounts(products)
+    @enhanced_cart = products_with_discounts.map {|product| { product:product, quantity: cart[product.id.to_s] } }
   end
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
-    enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
+    enhanced_cart.map {|entry| (entry[:product].discounted_price_cents || entry[:product].price_cents) * entry[:quantity]}.sum
   end
   helper_method :cart_subtotal_cents
 
--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -3,4 +3,14 @@ module SalesHelper
   def active_sale?
     Sale.active.any?
   end
+
+  def apply_discounts(products)
+    percent_off = Sale.active.maximum('percent_off')
+  
+    products.map { |product|
+      product.discounted_price_cents = product.price_cents * (100 - percent_off) / 100
+      product
+    }
+  end
+
 end
